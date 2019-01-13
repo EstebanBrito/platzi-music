a89869a27e51403a3f4a6d5553bce60673f1ab32
Esteban Brito <esteban.brito.borges@gmail.com>
2019-01-12 23:22:12 -0600

    Initial commit. Ready for deployment

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+//Application name 	platzi-music
+//API key 	899eb8791d7272f62f90288c7e99f9f8
+//Shared secret 	e112e6d05a684d0b737a5ed950848cd8
+//Registered to 	Usuario777
+
+export default{
+    appname: 'platzi-music',
+    apiKey: '899eb8791d7272f62f90288c7e99f9f8',
+    secret: 'e112e6d05a684d0b737a5ed950848cd8',
+    registeredTo: 'Usuario777'
+}
---
+++ src/components/Artist.vue
@@ -0,0 +1,23 @@
+<template>
+  <li>
+    <h2>
+      <a :href="artist.url" target="_blank">{{ artist.name }}</a>
+    </h2>
+    <img :src="artist.image[1]['#text']">
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'artist',
+  props: ['artist']
+}
+</script>
+
+<style scoped>
+li {
+  display: block;
+  margin: 10px 0 0;
+}
+</style>
+
